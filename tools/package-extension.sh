ab24c82181ddadd1bd01bc5df7d910916af59507
FlamedDogo99 <96555444+FlamedDogo99@users.noreply.github.com>
2025-10-10 10:42:41 -0600

    Fixed versioning, removed .DS_Store from build

--- tools/package-extension.sh
+++ tools/package-extension.sh
@@ -1,6 +1,8 @@
 node tools/build-extension.js "$1"
 cd build/temp || exit
 zip -r "MyMSU-$1.zip" .
+zip -d "MyMSU-$1.zip" /.DS_Store
+zip -d "MyMSU-$1.zip" \*/.DS_Store
 mv "MyMSU-$1.zip" "../"
 cd ../ || exit
 if [ -d "temp" ]; then rm -Rf "temp"; fi
