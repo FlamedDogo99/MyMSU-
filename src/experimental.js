472f377dc1350c64e8d9b0f8d4f99c10fea31665
FlamedDogo99 <96555444+FlamedDogo99@users.noreply.github.com>
2025-10-20 21:43:57 -0600

    Improved experimental changes stability

--- src/experimental.js
+++ src/experimental.js
@@ -16,40 +16,22 @@ window.fetch = async (...args) => {
   }
   const response = await originalFetch.call(this, ...args);
   overrideJson("api/locked-cards", (data) => {
-    data.cards = []; // Remove list of locked cards
+    if (config.method === "GET" || !config.method) data.cards = []; // Remove list of locked cards
     return data;
   });
   overrideJson("api/preferences", (data) => {
-    data.dashboard.cards = data.dashboard.cards.filter((item) => {
-      return !item.isLocked;
-    }); // Remove locked cards from preferences
+    if (config.method === "GET" || !config.method) {
+      data.dashboard.cards = data.dashboard.cards.filter((item) => {
+        return !item.isLocked;
+      }); // Remove locked cards from preferences
+    }
     return data;
   });
   overrideJson("api/dashboard-load", (data) => {
-    data.announcements = data.announcements.filter((item) => {
-      return item.externalLinkUrl !== "https://www.montana.edu/uit/mymsu";
-    }); // Remove "Welcome to MyMSU" alert
-    return data;
-  });
-  overrideJson("api/sessions", (data) => {
-    if (config.method === "get" || !config.method) {
-      if (data.remainingTime < 3) {
-        // When data.remainingTime is bellow 2, it displays the logout prompt
-        fetch(resource, {
-          header: { "Access-Control-Allow-Origin": "*" },
-          mode: "cors",
-          credentials: "include",
-          method: "post"
-        })
-          .then((response) => response.json())
-          .then((json) => {
-            if (json.remainingTime !== 60) {
-              console.warn("Unable to refresh session", data);
-              return data;
-            }
-          });
-      }
-      data.remainingTime = 60;
+    if (config.method === "GET" || !config.method) {
+      data.announcements = data.announcements.filter((item) => {
+        return item.externalLinkUrl !== "https://www.montana.edu/uit/mymsu";
+      }); // Remove "Welcome to MyMSU" alert
     }
     return data;
   });
